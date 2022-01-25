0d1e8fd65ac0ea55ae83df68ed4d4e08a733642e
AsterinGray <raymondtiandinata@gmail.com>
2022-01-25 16:07:33 +0700

    refactor(landing): add get server side props

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import { Button } from '@chakra-ui/button'
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input'
-import { Box, Container, Grid, Heading, Text } from '@chakra-ui/layout'
+import { Box, Container, Grid, Heading } from '@chakra-ui/layout'
 import Head from 'next/head'
-import MovieCard from '../components/MovieCard'
+import MovieCard from '../src/components/MovieCard'
 import { SearchIcon } from '@chakra-ui/icons'
+import Navbar from '../src/components/Navbar'
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+  )
+  const data = await res.json()
+  return {
+    props: { data },
+  }
+}
+
+const Home = ({ data }) => {
   return (
     <>
       <Head>
@@ -13,21 +24,8 @@ export default function Home() {
         <meta name="description" content="BNCC Movies" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar />
       <main>
-        <Container
-          maxW="container.lg"
-          paddingY="24px"
-          display="flex"
-          justifyContent="space-between"
-        >
-          <Text color="#2B6CB0" fontWeight="bold">
-            BNCC x tiket Movies
-          </Text>
-          <Text color="#2D3748" fontSize="14px" fontWeight="bold">
-            Login
-          </Text>
-        </Container>
         <Box backgroundColor="#2B6CB0">
           <Container maxW="container.lg" paddingY="32px">
             <Heading as="h1" fontSize="36px" color="white" marginBottom="32px">
@@ -37,15 +35,19 @@ export default function Home() {
               templateColumns={{
                 base: '1fr',
                 sm: 'repeat(3, 1fr)',
-                md: 'repeat(5, 1fr)',
+                md: 'repeat(4, 1fr)',
               }}
-              gap="12px"
+              columnGap="12px"
+              rowGap={`24px`}
             >
-              <MovieCard title="Venom Let There Be Carnage" />
-              <MovieCard title="Dune" />
-              <MovieCard title="Free Guy" />
-              <MovieCard title="The Vault" />
-              <MovieCard title="Snake Eyes G.I. Joe Origins" />
+              {data.results.map((result) => (
+                <MovieCard
+                  key={result.id}
+                  title={result.title}
+                  link={String(result.id)}
+                  image={result.poster_path}
+                />
+              ))}
             </Grid>
           </Container>
         </Box>
@@ -67,3 +69,5 @@ export default function Home() {
     </>
   )
 }
+
+export default Home
