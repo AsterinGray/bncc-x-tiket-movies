6411cbe56b86f38fac3621055933813272342ed1
AsterinGray <raymondtiandinata@gmail.com>
2022-02-20 23:23:36 +0700

    refactor(profile): remove user stats

--- src/components/pages/profile/ProfileBlock.jsx
+++ src/components/pages/profile/ProfileBlock.jsx
@@ -33,78 +33,9 @@ const ProfileBlock = () => {
             size="2xl"
             src={`https://image.tmdb.org/t/p/original/${data.avatar}`}
           />
-          <Box display="flex" flexDirection="column">
-            <Box display="flex" flexDirection="row">
-              <Text fontSize="4xl" color="white" fontWeight="bold" paddingX={4}>
-                {data.username}
-              </Text>
-              <Text
-                fontSize="xl"
-                color="white"
-                fontStyle="italic"
-                paddingTop={5}
-                opacity="0.6"
-              >
-                Member since June 2018
-              </Text>
-            </Box>
-            <Box>
-              <StatGroup>
-                <Stat paddingLeft={5}>
-                  <StatLabel color="white" fontSize="xl">
-                    Your Activity
-                  </StatLabel>
-                  <Box display="flex" flexDirection="row">
-                    <StatNumber color="white">100</StatNumber>
-                    <Text
-                      fontSize="xl"
-                      color="white"
-                      paddingTop={1}
-                      paddingLeft={2}
-                    >
-                      Movie
-                    </Text>
-                  </Box>
-                  <StatHelpText>
-                    <Box display="flex" flexDirection="row">
-                      <StatArrow type="increase" />
-                      <Text color="white" opacity="0.8">
-                        23.36%
-                      </Text>
-                    </Box>
-                    <Text color="white" opacity="0.6">
-                      than last week
-                    </Text>
-                  </StatHelpText>
-                </Stat>
-                <Center paddingTop={5} height="16">
-                  <Divider orientation="vertical" />
-                </Center>
-                <Stat paddingLeft={"16"}>
-                  <StatLabel color="white" fontSize="xl">
-                    Average Movie
-                  </StatLabel>
-                  <StatLabel paddingLeft={8} color="white" fontSize="xl">
-                    Rating
-                  </StatLabel>
-                  <CircularProgress
-                    paddingTop={2}
-                    paddingLeft={9}
-                    value={80}
-                    color="green.300"
-                  >
-                    <CircularProgressLabel
-                      paddingTop={2}
-                      paddingLeft={9}
-                      color="white"
-                    >
-                      80%
-                    </CircularProgressLabel>
-                  </CircularProgress>
-                </Stat>
-              </StatGroup>
-            </Box>
-          </Box>
+          <Text fontSize="4xl" color="white" fontWeight="bold" paddingX={4}>
+            {data.username}
+          </Text>
         </Container>
       </Box>
     </>
