917bcaf005bf6742457dabd750b66eed0e27663d
AsterinGray <raymondtiandinata@gmail.com>
2022-02-20 19:33:53 +0700

    refactor(store): add clearUser action

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -1,9 +1,4 @@
-import { 
-    Container, 
-    Text, 
-    Box, 
-    Grid 
-} from "@chakra-ui/react";
+import { Container, Text, Box, Grid } from "@chakra-ui/react";
 
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -11,35 +6,35 @@ import api from "../api";
 import { renderMovies } from "/src/utils/index";
 
 const WatchList = () => {
-    const { session_id, data } = useSelector((state) => state.user);
-    const [watchList, setWatchList] = useState([]);
+  const { session_id, data } = useSelector((state) => state.user);
+  const [watchList, setWatchList] = useState([]);
 
-    useEffect(() => {
-        if(session_id){
-            api.get(`/account/${data.id}/favorite/movies`, {
-              params: {
-                  session_id,
-                },
-          }).then((res) => setWatchList(res.data.results));
-        }
-    }, []);
-
-    const renderWatchList = () => {
-        return renderMovies(watchList);
+  useEffect(() => {
+    if (session_id) {
+      api
+        .get(`/account/${data.id}/favorite/movies`, {
+          params: {
+            session_id,
+          },
+        })
+        .then((res) => setWatchList(res.data.results));
     }
+  }, [session_id, data]);
 
-    return (
-        <>
-        <Box backgroundColor="white" paddingY={10} paddingTop={9}>
-            <Container maxW="container.xl">
-                <Text fontSize="4xl" color="black" fontWeight="bold">My Favorite Movies</Text>
-                <Grid>
-                    {watchList && renderWatchList()}
-                </Grid>
-            </Container>
-        </Box>
-        </>
-    );
+  const renderWatchList = () => {
+    return renderMovies(watchList);
+  };
+
+  return (
+    <Box backgroundColor="white" paddingY={10} paddingTop={9}>
+      <Container maxW="container.xl">
+        <Text fontSize="4xl" color="black" fontWeight="bold">
+          My Favorite Movies
+        </Text>
+        <Grid>{watchList && renderWatchList()}</Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default WatchList;
