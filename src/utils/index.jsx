e06fc5ab02780b8eec5d3651f85858c2c5d8fba1
AsterinGray <raymondtiandinata@gmail.com>
2022-02-20 14:50:21 +0700

    refactor(movie-card): refactor movie card component

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,26 +1,24 @@
 import { Grid } from "@chakra-ui/react";
 import MovieCard from "../components/MovieCard";
 
-export const renderMovies = (movies, altColor = false) => {
-  return (
-    <Grid
-      templateColumns={{
-        base: "1fr",
-        sm: "repeat(3, 1fr)",
-        md: "repeat(5, 1fr)",
-      }}
-      columnGap="12px"
-      rowGap={`48px`}
-    >
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          title={movie.title}
-          link={`/movie/${String(movie.id)}`}
-          image={movie.poster_path}
-          altColor={altColor}
-        />
-      ))}
-    </Grid>
-  );
-};
+export const renderMovies = (movies, altColor = false) => (
+  <Grid
+    templateColumns={{
+      base: "1fr",
+      sm: "repeat(3, 1fr)",
+      md: "repeat(5, 1fr)",
+    }}
+    columnGap="12px"
+    rowGap={`48px`}
+  >
+    {movies.map((movie) => (
+      <MovieCard
+        key={movie.id}
+        title={movie.title}
+        link={`/movie/${String(movie.id)}`}
+        image={movie.poster_path}
+        altColor={altColor}
+      />
+    ))}
+  </Grid>
+);
